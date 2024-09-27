be5a8b9e9ef4e0087d12f0b7f6ee8b5b14bf37e8
arahhman <abdurrahman@eskalink.com>
2024-09-27 22:53:45 +0700

    update

--- resources/js/components/member/HomeMain.vue
+++ resources/js/components/member/HomeMain.vue
@@ -575,19 +575,19 @@ export default {
             swiperBreakpointswawasan: {
                 320: {
                     slidesPerView: 1,
-                    spaceBetween: 10
+                    spaceBetween: 20
                 },
                 640: {
                     slidesPerView: 1,
-                    spaceBetween: 10
+                    spaceBetween: 20
                 },
                 768: {
                     slidesPerView: 1,
-                    spaceBetween: 10
+                    spaceBetween: 20
                 },
                 1024: {
                     slidesPerView: 2,
-                    spaceBetween: 10
+                    spaceBetween: 20
                 }
             },
             swiperBreakpointlayarpenulis: {
