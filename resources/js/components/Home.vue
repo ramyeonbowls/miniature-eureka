4e35040ed907188c266af8bbed1dd9f24ca1ffee
fedianpradanaa <apedh.pradana@gmail.com>
2024-10-30 17:19:36 +0700

    ubah label

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -130,7 +130,7 @@
                     <div class="col-12 col-xl-12">
                         <div class="card">
                             <div class="card-header">
-                                <h4>pertumbuhan Member</h4>
+                                <h4>Pertumbuhan Member</h4>
                             </div>
                             <div class="card-body">
                                 <div id="area"></div>
